e80fb9d873d425a29c5465245538e549b59055fd
songji.zeng <songji.zeng@outlook.com>
2024-09-07 13:30:16 +0800

    lifecycle

--- app/javascript/components/simple_greeting.js
+++ app/javascript/components/simple_greeting.js
@@ -2,25 +2,45 @@ import { LitElement, css, html } from 'lit'
 
 export class SimpleGreeting extends LitElement {
   static properties = {
-    name: {},
+    time: {},
   }
 
   // Define scoped styles right with your component, in plain CSS
   static styles = css`
     :host {
-      color: blue;
+      color: var(--text-color, blue);
     }
   `
 
   constructor() {
     super()
     // Declare reactive properties
-    this.name = 'World'
+    this.time = new Date()
+    this._timer = setInterval(() => { this.time = new Date() }, 1000)
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+
+    console.debug("connected")
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback()
+    clearInterval(this._timer)
+  }
+
+  willUpdate(changedProperties) {
+    console.debug("willUpdate", changedProperties)
+  }
+
+  updated(changedProperties) {
+    console.debug("updated:", changedProperties)
   }
 
   // Render the UI as a function of component state
   render() {
-    return html`<p>Hello, ${this.name}!</p>`
+    return html`<p>Time: ${this.time}!</p>`
   }
 }
 
